800d8fae6920d2fe3a026eeb64b0fd81020ef63d
Juan Esteban Villegas Arias <estebanari29@gmail.com>
2023-01-12 20:32:04 -0500

    lista la parte de las tarjetas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import "./App.css";
+import './App.scss';
+import OurCreations from './componentes/OurCreations';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className='App'>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/data/cards.js
@@ -0,0 +1,61 @@
+import deepEarth from '../images/desktop/image-deep-earth.jpg';
+import deepEarthM from '../images/mobile/image-deep-earth.jpg';
+import arcade from '../images/desktop/image-night-arcade.jpg';
+import arcadeM from '../images/mobile/image-night-arcade.jpg';
+import soccer from '../images/desktop/image-soccer-team.jpg';
+import soccerM from '../images/mobile/image-soccer-team.jpg';
+import grid from '../images/desktop/image-grid.jpg';
+import gridM from '../images/mobile/image-grid.jpg';
+import above from '../images/desktop/image-from-above.jpg';
+import aboveM from '../images/mobile/image-from-above.jpg';
+import pocket from '../images/desktop/image-pocket-borealis.jpg';
+import pocketM from '../images/mobile/image-pocket-borealis.jpg';
+import curiosity from '../images/desktop/image-curiosity.jpg';
+import curiosityM from '../images/mobile/image-curiosity.jpg';
+import fisheye from '../images/desktop/image-fisheye.jpg';
+import fisheyeM from '../images/mobile/image-fisheye.jpg';
+
+const listCards = [
+  {
+    title: 'DEEP EARTH',
+    imgDesktop: deepEarth,
+    imgMobile: deepEarthM,
+  },
+  {
+    title: 'NIGHT ARCADE',
+    imgDesktop: arcade,
+    imgMobile: arcadeM,
+  },
+  {
+    title: 'SOCCER TEAM VR',
+    imgDesktop: soccer,
+    imgMobile: soccerM,
+  },
+  {
+    title: 'THE GRID',
+    imgDesktop: grid,
+    imgMobile: gridM,
+  },
+  {
+    title: 'FROM UP ABOVE VR',
+    imgDesktop: above,
+    imgMobile: aboveM,
+  },
+  {
+    title: 'POCKET BOREALS',
+    imgDesktop: pocket,
+    imgMobile: pocketM,
+  },
+  {
+    title: 'THE CURIOSITY',
+    imgDesktop: curiosity,
+    imgMobile: curiosityM,
+  },
+  {
+    title: 'MAKE IT FISHEYE',
+    imgDesktop: fisheye,
+    imgMobile: fisheyeM,
+  },
+];
+
+export default listCards;
